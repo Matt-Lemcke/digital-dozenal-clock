bd7105af1f7c722a41ba0dd4b65a2b83d050208e
Matt-Lemcke <lemckematt@gmail.com>
2023-10-17 15:59:52 -0400

    Add lint for cpp files

--- firmware/tests/all_tests.cpp
+++ firmware/tests/all_tests.cpp
@@ -2,14 +2,9 @@
 #include "CppUTest/TestHarness_c.h"
 
 /** Test groups */
-TEST_GROUP_C_WRAPPER(FirstTestGroup)
-{
-
+TEST_GROUP_C_WRAPPER(FirstTestGroup) {
 };
 
 TEST_C_WRAPPER(FirstTestGroup, FirstTest);
 
-
-int main (int ac, char ** av){
-    return RUN_ALL_TESTS(ac, av);
-}
+int main(int ac, char** av) { return RUN_ALL_TESTS(ac, av); }
--- firmware/tests/test_dummy.c
+++ firmware/tests/test_dummy.c
@@ -1,9 +1,6 @@
 #include "CppUTest/TestHarness_c.h"
 #include "clock.h"
 
-TEST_GROUP_C_SETUP(FirstTestGroup) {
-};
+TEST_GROUP_C_SETUP(FirstTestGroup){};
 
-TEST_C(FirstTestGroup, FirstTest) {
-    CHECK_C(timer10Callback());
-}
+TEST_C(FirstTestGroup, FirstTest) { CHECK_C(timer10Callback()); }
