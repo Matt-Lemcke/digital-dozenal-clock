283faf4a64d95af04d21bb77f76505ef56f5edf6
Matt-Lemcke <lemckematt@gmail.com>
2023-11-24 13:01:00 -0500

    Initial project files

---
+++ firmware/stm32f0/Core/Inc/main.h
@@ -0,0 +1,150 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BTN11_IN_Pin GPIO_PIN_13
+#define BTN11_IN_GPIO_Port GPIOC
+#define BTN11_IN_EXTI_IRQn EXTI4_15_IRQn
+#define BTN10_IN_Pin GPIO_PIN_14
+#define BTN10_IN_GPIO_Port GPIOC
+#define BTN10_IN_EXTI_IRQn EXTI4_15_IRQn
+#define RTC_SQW_Pin GPIO_PIN_15
+#define RTC_SQW_GPIO_Port GPIOC
+#define RTC_SQW_EXTI_IRQn EXTI4_15_IRQn
+#define I2C_SDA_Pin GPIO_PIN_0
+#define I2C_SDA_GPIO_Port GPIOF
+#define I2C_SCL_Pin GPIO_PIN_1
+#define I2C_SCL_GPIO_Port GPIOF
+#define BTN1_IN_Pin GPIO_PIN_0
+#define BTN1_IN_GPIO_Port GPIOC
+#define BTN1_IN_EXTI_IRQn EXTI0_1_IRQn
+#define BTN2_IN_Pin GPIO_PIN_1
+#define BTN2_IN_GPIO_Port GPIOC
+#define BTN2_IN_EXTI_IRQn EXTI0_1_IRQn
+#define BTN3_IN_Pin GPIO_PIN_2
+#define BTN3_IN_GPIO_Port GPIOC
+#define BTN3_IN_EXTI_IRQn EXTI2_3_IRQn
+#define BTN4_IN_Pin GPIO_PIN_3
+#define BTN4_IN_GPIO_Port GPIOC
+#define BTN4_IN_EXTI_IRQn EXTI2_3_IRQn
+#define PHOTO_IN_Pin GPIO_PIN_0
+#define PHOTO_IN_GPIO_Port GPIOA
+#define BTN5_IN_Pin GPIO_PIN_4
+#define BTN5_IN_GPIO_Port GPIOA
+#define BTN5_IN_EXTI_IRQn EXTI4_15_IRQn
+#define BTN6_IN_Pin GPIO_PIN_5
+#define BTN6_IN_GPIO_Port GPIOA
+#define BTN6_IN_EXTI_IRQn EXTI4_15_IRQn
+#define BUZZER_PWM_Pin GPIO_PIN_6
+#define BUZZER_PWM_GPIO_Port GPIOA
+#define BTN9_IN_Pin GPIO_PIN_10
+#define BTN9_IN_GPIO_Port GPIOB
+#define BTN9_IN_EXTI_IRQn EXTI4_15_IRQn
+#define DSPL_A_Pin GPIO_PIN_12
+#define DSPL_A_GPIO_Port GPIOB
+#define DSPL_B_Pin GPIO_PIN_13
+#define DSPL_B_GPIO_Port GPIOB
+#define DSPL_C_Pin GPIO_PIN_14
+#define DSPL_C_GPIO_Port GPIOB
+#define DSPL_D_Pin GPIO_PIN_15
+#define DSPL_D_GPIO_Port GPIOB
+#define DSPL_E_Pin GPIO_PIN_6
+#define DSPL_E_GPIO_Port GPIOC
+#define DSPL_CLK_Pin GPIO_PIN_7
+#define DSPL_CLK_GPIO_Port GPIOC
+#define DSPL_LAT_Pin GPIO_PIN_8
+#define DSPL_LAT_GPIO_Port GPIOC
+#define DSPL_OE_Pin GPIO_PIN_9
+#define DSPL_OE_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_9
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_10
+#define USART_RX_GPIO_Port GPIOA
+#define DSPL_B2_Pin GPIO_PIN_10
+#define DSPL_B2_GPIO_Port GPIOC
+#define DSPL_B1_Pin GPIO_PIN_11
+#define DSPL_B1_GPIO_Port GPIOC
+#define DSPL_G2_Pin GPIO_PIN_12
+#define DSPL_G2_GPIO_Port GPIOC
+#define DSPL_G1_Pin GPIO_PIN_2
+#define DSPL_G1_GPIO_Port GPIOD
+#define DSPL_R2_Pin GPIO_PIN_3
+#define DSPL_R2_GPIO_Port GPIOB
+#define DSPL_R1_Pin GPIO_PIN_4
+#define DSPL_R1_GPIO_Port GPIOB
+#define BTN8_IN_Pin GPIO_PIN_6
+#define BTN8_IN_GPIO_Port GPIOB
+#define BTN8_IN_EXTI_IRQn EXTI4_15_IRQn
+#define BTN7_IN_Pin GPIO_PIN_7
+#define BTN7_IN_GPIO_Port GPIOB
+#define BTN7_IN_EXTI_IRQn EXTI4_15_IRQn
+#define BTN13_IN_Pin GPIO_PIN_8
+#define BTN13_IN_GPIO_Port GPIOB
+#define BTN13_IN_EXTI_IRQn EXTI4_15_IRQn
+#define BTN12_IN_Pin GPIO_PIN_9
+#define BTN12_IN_GPIO_Port GPIOB
+#define BTN12_IN_EXTI_IRQn EXTI4_15_IRQn
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ firmware/stm32f0/Core/Src/gpio.c
@@ -0,0 +1,126 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, DSPL_A_Pin|DSPL_B_Pin|DSPL_C_Pin|DSPL_D_Pin
+                          |DSPL_R2_Pin|DSPL_R1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, DSPL_E_Pin|DSPL_CLK_Pin|DSPL_LAT_Pin|DSPL_OE_Pin
+                          |DSPL_B2_Pin|DSPL_B1_Pin|DSPL_G2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(DSPL_G1_GPIO_Port, DSPL_G1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PCPin PCPin PCPin PCPin
+                           PCPin PCPin PCPin */
+  GPIO_InitStruct.Pin = BTN11_IN_Pin|BTN10_IN_Pin|RTC_SQW_Pin|BTN1_IN_Pin
+                          |BTN2_IN_Pin|BTN3_IN_Pin|BTN4_IN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin */
+  GPIO_InitStruct.Pin = BTN5_IN_Pin|BTN6_IN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin */
+  GPIO_InitStruct.Pin = BTN9_IN_Pin|BTN8_IN_Pin|BTN7_IN_Pin|BTN13_IN_Pin
+                          |BTN12_IN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin PBPin */
+  GPIO_InitStruct.Pin = DSPL_A_Pin|DSPL_B_Pin|DSPL_C_Pin|DSPL_D_Pin
+                          |DSPL_R2_Pin|DSPL_R1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PCPin PCPin PCPin PCPin
+                           PCPin PCPin PCPin */
+  GPIO_InitStruct.Pin = DSPL_E_Pin|DSPL_CLK_Pin|DSPL_LAT_Pin|DSPL_OE_Pin
+                          |DSPL_B2_Pin|DSPL_B1_Pin|DSPL_G2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = DSPL_G1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DSPL_G1_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI2_3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI2_3_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
